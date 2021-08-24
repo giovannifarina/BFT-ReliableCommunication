d891e96f38a0e44fab4fa2921c8e48aa6c52b3ff
giovannifarina <47982887+giovannifarina@users.noreply.github.com>
2021-08-24 14:17:57 +0200

    Updated to a newer version of networkx

--- kdiamond.py
+++ kdiamond.py
@@ -19,9 +19,9 @@ def generate_k_diamond(n,k):
     # base tree
     for i in range(1,k+1):
         Tree.add_edge(0,i)
-        Tree.node[i]['type'] = 'shared'
+        Tree.nodes[i]['type'] = 'shared'
         Shared_leaves.append(i)
-    Tree.node[0]['type'] = 'root'
+    Tree.nodes[0]['type'] = 'root'
 
     def Phase1_goToNextLevel():
         nonlocal free_node_label
@@ -33,16 +33,16 @@ def generate_k_diamond(n,k):
         Unshared_leaves = []
         Expanding_nodes.clear()
         next_expanding_node = Future_expanding_nodes.pop()
-        Tree.node[next_expanding_node]['type'] = 'internal'
+        Tree.nodes[next_expanding_node]['type'] = 'internal'
         shared_nodes_to_move = [y for x, y in Tree.out_edges(last_expanded_node) if
-                                Tree.node[y]['type'] == 'shared']
+                                Tree.nodes[y]['type'] == 'shared']
 
         Tree.remove_nodes_from(shared_nodes_to_move[:k - 2])
         for node in shared_nodes_to_move[:k - 2]:
             Tree.add_edge(next_expanding_node, node)
-            Tree.node[node]['type'] = 'shared'
+            Tree.nodes[node]['type'] = 'shared'
         Tree.add_edge(next_expanding_node, free_node_label)
-        Tree.node[free_node_label]['type'] = 'shared'
+        Tree.nodes[free_node_label]['type'] = 'shared'
         Expanding_nodes.append(next_expanding_node)
         Phase = 2
 
@@ -57,22 +57,22 @@ def generate_k_diamond(n,k):
         for node in Expanding_nodes:
             if Tree.degree(node) < k + k - 2:
                 Tree.add_edge(node, free_node_label)
-                Tree.node[free_node_label]['type'] = 'shared'
+                Tree.nodes[free_node_label]['type'] = 'shared'
                 last_expanded_node = node
                 added_shared_node_flag = True
                 break
         if not added_shared_node_flag:  # new internal and expanding
             if len(Future_expanding_nodes) > 0:
                 next_expanding_node = Future_expanding_nodes.pop()
-                Tree.node[next_expanding_node]['type'] = 'internal'
+                Tree.nodes[next_expanding_node]['type'] = 'internal'
                 shared_nodes_to_move = [y for x, y in Tree.out_edges(last_expanded_node) if
-                                        Tree.node[y]['type'] == 'shared']
+                                        Tree.nodes[y]['type'] == 'shared']
                 Tree.remove_nodes_from(shared_nodes_to_move[:k - 2])
                 for node in shared_nodes_to_move[:k - 2]:
                     Tree.add_edge(next_expanding_node, node)
-                    Tree.node[node]['type'] = 'shared'
+                    Tree.nodes[node]['type'] = 'shared'
                 Tree.add_edge(next_expanding_node, free_node_label)
-                Tree.node[free_node_label]['type'] = 'shared'
+                Tree.nodes[free_node_label]['type'] = 'shared'
                 Expanding_nodes.append(next_expanding_node)
                 last_expanded_node = next_expanding_node
             else:  # all unshared are internals now
@@ -88,10 +88,10 @@ def generate_k_diamond(n,k):
 
         if last_expanded_index < len(Expanding_nodes):
             node_to_expand = Expanding_nodes[last_expanded_index]
-            shared_nodes_to_move = [y for x, y in Tree.out_edges(node_to_expand) if Tree.node[y]['type'] == 'shared']
+            shared_nodes_to_move = [y for x, y in Tree.out_edges(node_to_expand) if Tree.nodes[y]['type'] == 'shared']
             Tree.remove_nodes_from(shared_nodes_to_move[:k - 1])
             Tree.add_edge(node_to_expand, free_node_label)
-            Tree.node[free_node_label]['type'] = 'unshared'
+            Tree.nodes[free_node_label]['type'] = 'unshared'
             Unshared_leaves.append(free_node_label)
             last_expanded_index += 1
         else:
@@ -111,7 +111,7 @@ def generate_k_diamond(n,k):
         for node in Expanding_nodes:
             if Tree.degree(node) < k + k - 2:
                 Tree.add_edge(node, free_node_label)
-                Tree.node[free_node_label]['type'] = 'shared'
+                Tree.nodes[free_node_label]['type'] = 'shared'
                 added_shared_node_flag = True
                 break
         if not added_shared_node_flag:
@@ -129,7 +129,7 @@ def generate_k_diamond(n,k):
         last_expanded_node = Expanding_nodes[-1]
         if Tree.degree(last_expanded_node) < k + k - 2:
             Tree.add_edge(last_expanded_node, free_node_label)
-            Tree.node[free_node_label]['type'] = 'shared'
+            Tree.nodes[free_node_label]['type'] = 'shared'
             if Tree.degree(last_expanded_node) == k + k - 2:
                 Phase = 1
 
@@ -142,14 +142,14 @@ def generate_k_diamond(n,k):
         if Phase == 0:  # first tree expansion
             if Tree.degree(Expanding_nodes[0]) < k + k - 2:
                 Tree.add_edge(Expanding_nodes[0], free_node_label)
-                Tree.node[free_node_label]['type'] = 'shared'
+                Tree.nodes[free_node_label]['type'] = 'shared'
                 Shared_leaves.append(free_node_label)
             elif len(Shared_leaves) >= k - 1:
                 for i in range(k - 1):
                     node_to_remove = Shared_leaves.pop()
                     Tree.remove_node(node_to_remove)
                 Tree.add_edge(Expanding_nodes[0], free_node_label)
-                Tree.node[free_node_label]['type'] = 'unshared'
+                Tree.nodes[free_node_label]['type'] = 'unshared'
                 Unshared_leaves.append(free_node_label)
             else:
                 Phase = 1   # increase hight
@@ -179,47 +179,47 @@ def generate_k_diamond(n,k):
     node = 0
     types_inserted = set()
     while Tree.out_degree(node) > 0:
-        types_inserted.add(Tree.node[node]['type'])
-        Nodes_To_Analize.append([node, Tree.node[node]['type']])
+        types_inserted.add(Tree.nodes[node]['type'])
+        Nodes_To_Analize.append([node, Tree.nodes[node]['type']])
         node = next(Tree.neighbors(node))
 
 
     for node in Tree.nodes():
         mapping = []
-        if Tree.node[node]['type'] != 'shared':
+        if Tree.nodes[node]['type'] != 'shared':
 
-            if Tree.node[node]['type'] == 'unshared' and 'unshared' not in types_inserted:
-                types_inserted.add(Tree.node[node]['type'])
-                Nodes_To_Analize.append([node, Tree.node[node]['type']])
+            if Tree.nodes[node]['type'] == 'unshared' and 'unshared' not in types_inserted:
+                types_inserted.add(Tree.nodes[node]['type'])
+                Nodes_To_Analize.append([node, Tree.nodes[node]['type']])
 
             mapping.append(node)
             G.add_node(node)
-            G.node[node]['type'] = Tree.node[node]['type']
+            G.nodes[node]['type'] = Tree.nodes[node]['type']
             for i in range(k-1):
                 mapping.append(node_label)
                 G.add_node(node_label)
-                G.node[node_label]['type'] = Tree.node[node]['type']
+                G.nodes[node_label]['type'] = Tree.nodes[node]['type']
                 node_label += 1
             node_mapping[node] = mapping
         else:
 
             if 'shared' not in types_inserted:
-                types_inserted.add(Tree.node[node]['type'])
-                Nodes_To_Analize.append([node, Tree.node[node]['type']])
+                types_inserted.add(Tree.nodes[node]['type'])
+                Nodes_To_Analize.append([node, Tree.nodes[node]['type']])
 
             for i in range(k):
                 mapping.append(node)
             node_mapping[node] = mapping
             G.add_node(node)
-            G.node[node]['type'] = Tree.node[node]['type']
+            G.nodes[node]['type'] = Tree.nodes[node]['type']
 
     for node in Tree.nodes():
-        if Tree.node[node]['type'] == 'unshared':
+        if Tree.nodes[node]['type'] == 'unshared':
             for i in range(k):
                 for j in range(k):
                     if i<j:
                         G.add_edge(node_mapping[node][i], node_mapping[node][j])
-        elif Tree.node[node]['type'] != 'shared': # root or internal
+        elif Tree.nodes[node]['type'] != 'shared': # root or internal
             for edge in Tree.out_edges(node):
                 for i in range(k):
                     G.add_edge(node_mapping[edge[0]][i], node_mapping[edge[1]][i])
